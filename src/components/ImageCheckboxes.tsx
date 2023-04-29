ab0e63946c8faf103cac39a3ea740fd677cc15b6
Ayush Jain <ayushjain2205@gmail.com>
2023-04-29 22:57:13 +0530

    Add create page

---
+++ src/components/ImageCheckboxes.tsx
@@ -0,0 +1,148 @@
+import {
+  UnstyledButton,
+  Checkbox,
+  Text,
+  Image,
+  SimpleGrid,
+  createStyles,
+  rem,
+} from "@mantine/core";
+import { useUncontrolled } from "@mantine/hooks";
+
+const useStyles = createStyles((theme, { checked }: { checked: boolean }) => ({
+  button: {
+    display: "flex",
+    alignItems: "center",
+    width: "100%",
+    transition: "background-color 150ms ease, border-color 150ms ease",
+    border: `${rem(1)} solid ${
+      checked
+        ? theme.fn.variant({ variant: "outline", color: theme.primaryColor })
+            .border
+        : theme.colorScheme === "dark"
+        ? theme.colors.dark[8]
+        : theme.colors.gray[3]
+    }`,
+    borderRadius: theme.radius.sm,
+    padding: theme.spacing.sm,
+    backgroundColor: checked
+      ? theme.fn.variant({ variant: "light", color: theme.primaryColor })
+          .background
+      : theme.colorScheme === "dark"
+      ? theme.colors.dark[8]
+      : theme.white,
+  },
+
+  body: {
+    flex: 1,
+    marginLeft: theme.spacing.md,
+  },
+}));
+
+interface ImageCheckboxProps {
+  checked?: boolean;
+  defaultChecked?: boolean;
+  onChange?(checked: boolean): void;
+  title: string;
+  description: string;
+  extension: string;
+}
+
+export function ImageCheckbox({
+  checked,
+  defaultChecked,
+  onChange,
+  title,
+  description,
+  className,
+  extension,
+  ...others
+}: ImageCheckboxProps &
+  Omit<React.ComponentPropsWithoutRef<"button">, keyof ImageCheckboxProps>) {
+  const [value, handleChange] = useUncontrolled({
+    value: checked,
+    defaultValue: defaultChecked,
+    finalValue: false,
+    onChange,
+  });
+
+  const { classes, cx } = useStyles({ checked: value });
+
+  return (
+    <UnstyledButton
+      {...others}
+      onClick={() => handleChange(!value)}
+      className={cx(classes.button, className)}
+    >
+      <div className="min-h-[40px] min-w-[40px] rounded-full bg-black text-xs flex justify-center items-center bg-info text-white">
+        {extension}
+      </div>
+
+      <div className={classes.body}>
+        <Text color="dimmed" size="xs" sx={{ lineHeight: 1 }} mb={5}>
+          {description}
+        </Text>
+        <Text weight={500} size="sm" sx={{ lineHeight: 1 }}>
+          {title}
+        </Text>
+      </div>
+
+      <Checkbox
+        checked={value}
+        onChange={() => {}}
+        tabIndex={-1}
+        styles={{ input: { cursor: "pointer" } }}
+      />
+    </UnstyledButton>
+  );
+}
+
+const mockdata = [
+  {
+    description: "Books, guides",
+    title: "PDFs",
+    extension: "png",
+  },
+  {
+    description: "Finances",
+    title: "Excel doc",
+    extension: "xls",
+  },
+  {
+    description: "Numbers",
+    title: "CSV File",
+    extension: "csv",
+  },
+  {
+    description: "Webpages",
+    title: "Links",
+    extension: "www",
+  },
+  {
+    description: "Presentations",
+    title: "PPT",
+    extension: "ppt",
+  },
+  {
+    description: "Webpages",
+    title: "Links",
+    extension: "www",
+  },
+];
+
+export function ImageCheckboxes() {
+  const items = mockdata.map((item) => (
+    <ImageCheckbox {...item} key={item.title} />
+  ));
+  return (
+    <SimpleGrid
+      cols={4}
+      breakpoints={[
+        { maxWidth: "md", cols: 2 },
+        { maxWidth: "sm", cols: 1 },
+      ]}
+    >
+      {items}
+    </SimpleGrid>
+  );
+}
--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import Layout from "@/components/Layout";
+import { ImageCheckboxes } from "@/components/ImageCheckboxes";
+import { FloatingLabelInput } from "@/components/FloatingLabel";
+import Link from "next/link";
 
 const create = () => {
-  return <Layout>create</Layout>;
+  return (
+    <Layout>
+      <h1 className="text-xl">Create Knowledge Base</h1>
+      <br />
+      <FloatingLabelInput
+        label="Name"
+        placeholder="What would you like to call your chatbot?"
+      />
+      <br />
+      <FloatingLabelInput
+        label="Description"
+        placeholder="What does your chatbot do?"
+      />
+      <br />
+      <br />
+      <h1 className="text-l mb-2">Choose sources:</h1>
+      <ImageCheckboxes />
+      <Link href="data">
+        <button className="btn btn-primary btn-block mt-32">Load Data</button>
+      </Link>
+    </Layout>
+  );
 };
 
 export default create;
